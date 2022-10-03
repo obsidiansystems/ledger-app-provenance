2c5ff030b38be902ff7245121bfa264e362cf1d7
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-10-03 22:10:36 +0000

    Update bip32 path handling to be more tolerant.

--- rust-app/src/implementation.rs
+++ rust-app/src/implementation.rs
@@ -354,20 +354,15 @@ any_of! {
     }
     }
 
-const BIP_PATH_PARSER : impl AsyncParser<Bip32Key, ByteStream> + HasOutput<Bip32Key, Output=ArrayVec<u32, 10>>= // Action(
-    SubInterp(DefaultInterp); /*,
-    // And ask the user if this is the key the meant to sign with:
-    mkfn(|path: &ArrayVec<u32, 10>, destination: &mut _| {
-
-        /*let privkey = get_private_key(path).ok()?;
-        let pubkey = get_pubkey(path).ok()?; // Redoing work here; fix.
-        let pkh = get_pkh(pubkey).ok()?;
-
-        write_scroller("With PKH", |w| Ok(write!(w, "{}", pkh)?))?;*/
-
-        *destination = Some(path.clone());
-        Some(())
-    }));*/
+const BIP_PATH_PARSER : impl AsyncParser<Bip32Key, ByteStream> + HasOutput<Bip32Key, Output=ArrayVec<u32, 10>>=
+    Action(SubInterp(DefaultInterp),
+    |path: ArrayVec<u32, 10>| {
+        if path.len()<2 || path[0] != 0x8000002c || path[1] != 0x800001f9 {
+            None
+        } else {
+            Some(path)
+        }
+    });
 
 // #[rustc_layout(debug)]
 // type Q<'c> = <Sign as AsyncAPDU>::State<'c>;
