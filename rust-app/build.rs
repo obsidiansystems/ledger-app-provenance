578f8f13f2fa91f65e240185ba4659820cbf5070
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-11-29 22:51:15 +0000

    WIP: make the relocations section size adjustable.

--- rust-app/build.rs
+++ rust-app/build.rs
@@ -6,6 +6,15 @@ use tempfile;
 
 fn main() -> std::io::Result<()> {
     println!("cargo:rerun-if-changed=script.ld");
+    let profile = std::env::var("PROFILE").unwrap();
+    let debug_print = std::env::var("CARGO_FEATURE_SPECULOS").is_ok();
+    let extra_debug_print = std::env::var("CARGO_FEATURE_EXTRA_DEBUG").is_ok();
+    let reloc_size = match (profile.as_str(), debug_print, extra_debug_print) {
+        ("release", false, false) => 768,
+        (_, _, true) => 1024*10,
+        _ => 1024*7,
+    };
+    println!("cargo:rustc-link-arg=--defsym=_reloc_size={}", reloc_size);
 
     // Cosmos repo path
     let cosmos = env::var("COSMOS_SDK").unwrap();
