2bf1317925d77a5864583b784d7cee55340a1b97
Jonathan D.K. Gibbons <jonored@gmail.com>
2022-12-09 21:10:23 +0000

    Bump nanos-ui crate to fix bug, configure s+ size.

--- rust-app/build.rs
+++ rust-app/build.rs
@@ -9,9 +9,12 @@ fn main() -> std::io::Result<()> {
     let profile = std::env::var("PROFILE").unwrap();
     let debug_print = std::env::var("CARGO_FEATURE_SPECULOS").is_ok();
     let extra_debug_print = std::env::var("CARGO_FEATURE_EXTRA_DEBUG").is_ok();
-    let reloc_size = match (profile.as_str(), debug_print, extra_debug_print) {
-        ("release", false, false) => 768,
-        (_, _, true) => 1024*10,
+    let target = std::env::var("CARGO_CFG_TARGET_OS").unwrap();
+    let reloc_size = match (profile.as_str(), debug_print, extra_debug_print, target.as_str()) {
+        ("release", false, false, "nanosplus") => 1312,
+        ("release", false, false, "nanos") => 768,
+        ("release", false, false, "nanox") => 768,
+        (_, _, true, _) => 1024*10,
         _ => 1024*7,
     };
     println!("cargo:rustc-link-arg=--defsym=_reloc_size={}", reloc_size);
