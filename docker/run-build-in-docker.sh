2a4b2f63adff851188faad88382f4d1a0c5086a5
Divam <dfordivam@gmail.com>
2023-07-18 12:14:36 +0900

    Revert "Provide COSMOS_SDK, buf and PROTO_INCLUDE in docker build"
    This reverts commit 777cbaabc0b0d2c428268cfdcfdd802d82b3893e.

--- docker/run-build-in-docker.sh
+++ docker/run-build-in-docker.sh
@@ -13,19 +13,6 @@ PATH=$RUST_NANOS_SDK/scripts:$PATH
 export OBJCOPY="llvm-objcopy"
 export NM="llvm-nm"
 
-# The following are needed for protobuf related codegen
-export COSMOS_SDK=`mktemp -d`
-git clone https://github.com/cosmos/cosmos-sdk.git "$COSMOS_SDK"
-cd "$COSMOS_SDK"; git checkout 518003ec29455e0eeb3b46219a940d32b860973f; cd -
-
-BIN="/usr/local/bin" && \
-    VERSION="1.14.0" && \
-    curl -sSL \
-         "https://github.com/bufbuild/buf/releases/download/v${VERSION}/buf-$(uname -s)-$(uname -m)" \-o "${BIN}/buf" && \
-    chmod +x "${BIN}/buf"
-
-export PROTO_INCLUDE=$(readlink -e $(dirname $(which protoc))/../include)
-
 cd rust-app
 
 for device in nanos nanosplus nanox
